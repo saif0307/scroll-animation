fa586950cdb372d0b351fea8c936b833370d1009
mohsinidrees2817 <111157210+mohsinidrees2817@users.noreply.github.com>
2022-10-24 20:19:55 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useRef, useEffect, useState,useLayoutEffect } from "react";
-import { Parallax, ParallaxLayer } from "@react-spring/parallax";
-import SmothScrolling from "./Components/SmothScrolling";
+
 
 import {
   ScrollContainer,
@@ -26,44 +25,69 @@ function App() {
   const [secondimageWidth, setSecondimagewidth] = useState(1);
   const [cubewidth, setCubewidth] = useState(0);
   const [imageScale ,setImagescale]= useState(false);
+  const [topImagePadding ,setTopImagePadding] = useState(-40);
+  const [secondImagePadding, setSecondimagePadding] = useState(90);
+  const [cubePadding, setCubepadding]  = useState(20);
 
   const ref = useRef();
 
-  const onScroll = (offset,height) => {
-    // e.preventDefault();
-    console.log('fdg')
+  const onScroll = () => {
     const Scrolled = document.documentElement.scrollTop;
     const MaxHeight =
       document.documentElement.scrollHeight -
       document.documentElement.clientHeight;
     const ScrollPercent = (Scrolled / MaxHeight) * 100;
-    // console.log(ScrollPercent,'vsdjvjbs');
-    if (ScrollPercent < 10) {
+    console.log(ScrollPercent,'scrollpercent')
+    
+    if (ScrollPercent < 10 ) {
       setTopimagewidth(250);
+      setTopImagePadding(-50);
       setImagescale(false);
-    } else if (ScrollPercent < 11) {
-      setTopimagewidth(0);
+    } else if (ScrollPercent < 11 ) {
       setImagescale(true);
-      setSecondimagewidth(1);
-    } else if (ScrollPercent < 23) {
-      setSecondimagewidth(1);
+      setTopimagewidth(50);
+      setTopImagePadding(-25);
+      setSecondimagewidth(10);
+
+    } else if (ScrollPercent < 14) {
+      setTopimagewidth(20);
+      setTopImagePadding(-25);
+    } else if (ScrollPercent < 21) {
+      setSecondimagewidth(40);
+      setImagescale(true);
+      setSecondimagePadding(50);
     } else if (ScrollPercent > 95) {
       setSecondimagewidth(250);
-    } else if (ScrollPercent > 68) {
+    } 
+    else if (ScrollPercent > 71) {
       setSecondimagewidth(1);
-      setCubewidth(1);
+      setCubewidth(0);
+      setCubepadding(33);
+    }else if (ScrollPercent > 70) {
+      setSecondimagewidth(1);
+      setCubewidth(2);
+      setCubepadding(33);
     } else if (ScrollPercent > 52) {
       setSecondimagewidth(250);
       setCubewidth(9);
-    } else if (ScrollPercent > 40) {
-      setSecondimagewidth(1);
+      setCubepadding(20);
+    } 
+    else if (ScrollPercent > 40) {
+      setSecondimagewidth(20);
+      setSecondimagePadding(65);
       setCubewidth(1);
-    } else if (ScrollPercent > 23) {
+    }else if (ScrollPercent > 38) {
+      setSecondimagewidth(50);
+      setSecondimagePadding(60);
+      setCubewidth(1);
+    } 
+else if (ScrollPercent > 21) {
       setSecondimagewidth(250);
+      setImagescale(false);
+      setSecondimagePadding(40);
     }
   };
 
-console.log(imageScale,'image saclae')
 
   useEffect(()=>{
     window.addEventListener("scroll", onScroll);
@@ -94,31 +118,27 @@ console.log(imageScale,'image saclae')
     },[])  
   
 
-  // useLayoutEffect(() => {
-  //   console.log("hiiii");
-    
-  // }, []);
-  console.log(window.innerHeight,'lkk')
+
   return (
     <div >
-    <div className="relative z-50">
-      <ScrollContainer>
-        <ScrollPage page={0}>
-          <Animator animation={batch(Sticky(50, -50), MoveOut(0, window.innerHeight*1.5))}>
+    <div className="">
+      <ScrollContainer className="transition-all duration-[1000] ">
+        <ScrollPage page={0} className='z-20'>
+          <Animator animation={batch(Sticky(50, topImagePadding), MoveOut(-20, window.innerHeight*1.4))}>
             <img
               src="./Images/topdesign1.png"
               // style={{ transform: `scale(0.${topimageWidth})` }}
-              className={`transition-all ${imageScale ? `firstImage` : `animate-wiggle` }  duration-[200ms] ease-linear`}
+              className={`transition-all duration-1000 ${imageScale ? 'transition': 'animate-wiggle' }`}
               alt=""
-              width={250}
+              width={topimageWidth}
             />
-          </Animator>
+          </Animator> 
         </ScrollPage>
-        <ScrollPage page={1}>
+        <ScrollPage page={1} className='z-0'>
           <Animator
             animation={batch(
               Sticky(70, 70),
-              MoveIn(2000, 0),
+              MoveIn(500, 0),
               MoveOut(0, -1000),
               Fade()
             )}
@@ -129,7 +149,7 @@ console.log(imageScale,'image saclae')
               A PURIFED ECOSYSTEM FOR WEB3
             </p>
           </Animator>
-          <Animator animation={batch(Sticky(50, 80), MoveOut(0, -1000))}>
+          <Animator animation={batch(Sticky(50, 80), MoveOut(0, -1000)) }>
             <img
               src="./Images/1stbottom.png"
               className=""
@@ -142,16 +162,16 @@ console.log(imageScale,'image saclae')
         <ScrollPage page={2}>
           <Animator
             animation={batch(
-              Sticky(50, 30),
+              Sticky(50, secondImagePadding),
               FadeIn(),
-              MoveIn(10, 600),
-              MoveOut(0, 230)
+              MoveIn(20, 200),
+              MoveOut(0, 200)
             )}
           >
             <img
               src="./Images/2ndpgdesign.png"
               // style={{ transform: `scale(0.${secondimageWidth})` }}
-              className={`transition-all animate-wiggle duration-[500ms] ease-in`}
+              className={` ${imageScale ? 'transition-all': 'animate-wiggle'}`}
               alt=""
               width={secondimageWidth}
             />
@@ -161,11 +181,10 @@ console.log(imageScale,'image saclae')
           <Animator
             animation={batch(
               Sticky(30, 70),
-              MoveIn(-1000, 0),
+              MoveIn(-300, 0),
               MoveOut(0, -1000),
               Fade()
-            )}
-          >
+            )}  >
             <p
               className={`text-3xl text-[#2a60a5] font-bold transition-all duration-[2500ms] ease-in`}
             >
@@ -174,7 +193,7 @@ console.log(imageScale,'image saclae')
           </Animator>
 
           <Animator
-            animation={batch(Sticky(50, 85), MoveOut(0, -1000), FadeIn())}
+            animation={batch(Sticky(50, 90), MoveOut(0, -1000), FadeIn())}
           >
             <img
               src="./Images/2ndpgbase.png"
@@ -188,7 +207,7 @@ console.log(imageScale,'image saclae')
         <ScrollPage page={4}>
           <Animator
             animation={batch(
-              Sticky(50, 20),
+              Sticky(50, cubePadding),
               FadeIn(),
               MoveIn(-10, 600),
               MoveOut(0, 300)
@@ -196,7 +215,7 @@ console.log(imageScale,'image saclae')
           >
             <div
               style={{ transform: `scale(0.${cubewidth})` }}
-              className={`flex flex-col justify-center items-center transition-all duration-[200ms] ease-linear`}
+              className={`flex flex-col z-10 justify-center items-center transition-all duration-[200ms] ease-linear`}
             >
               <div className="flex justify-center items-end gap-[2px]">
                 <img
@@ -265,7 +284,7 @@ console.log(imageScale,'image saclae')
           <Animator
             animation={batch(Sticky(50, 90), MoveOut(0, -600), FadeIn())}
           >
-            <img src="./Images/cubebase.png" className="" alt="" width={400} />
+            <img src="./Images/cubebase.png" className="z-0" alt="" width={400} />
           </Animator>
           <Animator animation={batch(Sticky(76, 90), MoveOut(0, -600), Fade())}>
             <p
@@ -300,7 +319,7 @@ console.log(imageScale,'image saclae')
                 <div>
                   <img
                     src="./Images/bottomdesign1.png"
-                    className="transition-all duration-[500ms] ease-in"
+                    className="transition-all duration-[500ms]  ease-in"
                     alt=""
                     width={120}
                     // style={{ transform: `scale(0.${secondimageWidth})` }}
@@ -309,7 +328,7 @@ console.log(imageScale,'image saclae')
                 <div>
                   <img
                     src="./Images/bottomdesign3.png"
-                    className="transition-all  duration-[500ms]  ease-in"
+                    className="transition-all  duration-[500ms]   ease-in"
                     alt=""
                     width={120}
                     // style={{ transform: `scale(0.${secondimageWidth})` }}
@@ -318,7 +337,7 @@ console.log(imageScale,'image saclae')
                 <div>
                   <img
                     src="./Images/bottomdesign2.png"
-                    className="transition-all duration-[500ms]   ease-in"
+                    className="transition-all duration-[500ms]    ease-in"
                     alt=""
                     width={120}
                     // style={{ transform: `scale(0.${secondimageWidth})` }}
